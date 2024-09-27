c71df55d24272a2891f84f000a0d3cb8f82d8537
UprightBiswa <djdas000000@gmail.com>
2024-09-27 18:14:04 +0530

    Fix deployment issue

--- lib/app/data/constants/api_base_url.dart
+++ lib/app/data/constants/api_base_url.dart
@@ -1,7 +1,7 @@
 class BaseURL {
-  static const String baseUrl = 'http://192.168.29.48:8000/api/';
+  // static const String baseUrl = 'http://192.168.29.48:8000/api/';
+  static const String baseUrl = 'https://cs.jobisyour.com/api/';
   // const String baseURL = "http://10.0.2.2:8000/api";
 
 //  const String baseURL = "http://192.168.29.48/api";
-
 }
